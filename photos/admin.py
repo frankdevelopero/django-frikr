2af278369883984162eaf3718d52c1c1011bd7dc
Frank <frank.lopezcoras@gmail.com>
2018-11-29 21:52:33 -0500

    Personalizando la administrador de Django

--- photos/admin.py
+++ photos/admin.py
@@ -1,5 +1,32 @@
 from django.contrib import admin
 from photos.models import Photo
 
-# Register your models here.
-admin.site.register(Photo)
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner_name', 'licence', 'visivility')
+    list_filter = ('licence', 'visivility')
+    search_fields = ('name', 'description')
+
+    def owner_name(self, obj):
+        return obj.owner.first_name + ' ' + obj.owner.last_name
+
+    owner_name.short_description = 'Photo Owner'  # Atributo
+    owner_name.admin_order_field = 'owner'
+
+    fieldsets = (
+        (None, {
+            'fields': ('name',),
+            'classes': ('wide',)
+        }),
+        ('description & Author',{
+            'fields': ('description', 'owner'),
+            'classes': ('wide', )
+        }),
+        ('Extra',{
+            'fields': ('url', 'licence', 'visivility'),
+            'classes': ('wide', 'collapse')
+        })
+    )
+
+
+admin.site.register(Photo, PhotoAdmin)
